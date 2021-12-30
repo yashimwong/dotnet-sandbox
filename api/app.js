b99a768b581d589f4cd40516ef8b83a538e3f1e6
yashimwong <yashimwong@gmail.com>
2021-12-30 21:20:27 +0800

    feat: completed endpoints

--- api/app.js
+++ api/app.js
@@ -15,11 +15,17 @@ app.get("/api/v1/tours", (req, res) => {
     });
 });
 
-app.get("/api/v1/tours/:id", (req, res) => {
-    const { id } = req.params;
+const findTour = (id) => {
     const found_tour = tours.find((t) => t.id === Number(id));
     const data = found_tour ? found_tour : { status: "error", message: "Tour not found" };
-    res.status(200).json(data);
+    const status_code = found_tour ? 200 : 404;
+    return { data, status_code };
+};
+
+app.get("/api/v1/tours/:id", (req, res) => {
+    const { id } = req.params;
+    const { data, status_code } = findTour(id);
+    res.status(status_code).json(data);
 });
 
 app.post("/api/v1/tours", (req, res) => {
@@ -34,6 +40,48 @@ app.post("/api/v1/tours", (req, res) => {
             },
         });
     });
+});
+
+app.patch("/api/v1/tours/:id", (req, res) => {
+    const { id } = req.params;
+    const { data, status_code } = findTour(id);
+    if (status_code === 404) {
+        res.status(status_code).json(data);
+    } else {
+        const updated_tour = { ...req.body, id: data.id };
+        const new_tours = tours.map((tour) => {
+            if (tour.id === data.id) {
+                return updated_tour;
+            }
+            return tour;
+        });
+        fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(new_tours), (err) => {
+            res.status(201).json({
+                status: "success",
+                data: {
+                    tour: updated_tour,
+                },
+            });
+        });
+    }
+});
+
+app.delete("/api/v1/tours/:id", (req, res) => {
+    const { id } = req.params;
+    const { data, status_code } = findTour(id);
+    if (status_code === 404) {
+        res.status(status_code).json(data);
+    } else {
+        const new_tours = tours.filter((tour) => tour.id !== data.id);
+        fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(new_tours), (err) => {
+            res.status(201).json({
+                status: "success",
+                data: {
+                    tour: data,
+                },
+            });
+        });
+    }
 });
 
 const port = 3000;
